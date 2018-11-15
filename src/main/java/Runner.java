cc91613050ae1faeb1da37609922e940b60ec66e
Administrator <rkrakowski85@gmail.com>
2018-11-15 14:37:24 +0100

    task list -1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,20 +1,31 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        List<Long> list =  Arrays.asList(10L,25L,2488L,0L,3L,9L);
+        List<Long> list = Arrays.asList(10L, 25L, 2488L, 0L, 3L, 9L);
         System.out.println("Max long element: " + TaskLongs.findMaxElement(list));
         System.out.println("Min long element: " + TaskLongs.findMinElement(list));
         System.out.println("Medianaof list: " + TaskLongs.findMedianElement(list));
-        System.out.println("Longs Greater Then: " + TaskLongs.countLongsGreaterThen(list,24));
+        System.out.println("Longs Greater Then: " + TaskLongs.countLongsGreaterThen(list, 24));
 
-        int[] array = new int[]{5,8,99,1,1,66,9};
+        int[] array = new int[]{5, 8, 99, 1, 1, 66, 9};
         System.out.println("Max int in array: " + TaskInts.findMaxElement(array));
         System.out.println("Min int in array: " + TaskInts.findMinElement(array));
-        System.out.println("Min int in array on x possitions: " + TaskInts.findMinElemena(array,3));
-        System.out.println("Sum array: " + TaskInts.sum(array));
+        System.out.println("Min int in array on x possitions: " + TaskInts.findMinElemena(array, 3));
+        System.out.println("Sum of array: " + TaskInts.sum(array));
+
+        List<Integer> list1 = Arrays.asList(2, 2, 2, 2, 15, 0, 30);
+        ArrayList arrayList = TaskLists.removeDuplicates(list1);
+        System.out.println("Removind duplicates...");
+        TaskLists.printList(arrayList);
+        List<String> listString = Arrays.asList("ala", "ma", "kota", "ale","nie","ma","psa");
+        //System.out.println(TaskLists.concatenateStrings(listString));
+        //List prefixList = TaskLists.filterByPrefix(listString, "al");
+        List prefixList  = TaskLists.filterBySuffix(listString,"ma");
+        //System.out.println(TaskLists.concatenateStrings(prefixList));
 
 
     }
--- src/main/java/TaskLists.java
+++ src/main/java/TaskLists.java
@@ -1,14 +1,37 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TaskLists {
     /*
-    Zaimplementuj metodę removeDuplicates tak, aby zwracała nową listę, z której zostały usunięte podwójne wartości
-    (jeżeli jedna wartość występowała 2 lub więcej razy, to w wynikowej liście powinna znajdować się tylko raz)
-Zaimplementuj metodę concatenateStrings tak, aby zwracała wszystkie wartości String zapisane w postaci jednego długiego
-ciągu znaków (jeżeli na liście znajdowały się String o wartościach "aaa", "bbb", "ccc", to wynikiem powinien być jeden
-String "aaabbbccc")
-Zaimplementuj metodę filterByPrefix tak, aby zwracała tylko te wartości, które zaczynają się od danego tekstu
-Zaimplementuj metodę filterBySuffix tak, aby zwracała tylko te wartości, które kończą się na dany tekst
 Zaimplementuj metodę groupByLevelValue tak, aby zwracała mapę, w której kluczem będzie numer zapisany po słowie "Level-",
 a wartością będzie lista obiektów typu String zaczynająca się od tego levelu. Obiekty, które nie zaczynają się od słowa "Level"
 powinny zostać pominięte.
      */
+    public static void printList(List list) {
+        for (Object l : list) {
+            System.out.print(l + "\t");
+        }
+        System.out.println();
+    }
+
+    public static ArrayList removeDuplicates(List list) {
+        return new ArrayList(new HashSet(list));
+    }
+
+    public static String concatenateStrings(List list) {
+        return list.stream().collect(Collectors.joining()).toString();
+    }
+
+    public static List filterByPrefix(List<String> list, String prefix) {
+        return list.stream().filter(x -> x.startsWith(prefix)).collect(Collectors.toList());
+    }
+    public static List filterBySuffix(List<String> list, String prefix) {
+        return list.stream().filter(x -> x.endsWith(prefix)).collect(Collectors.toList());
+    }
+
+
+
+
 }
